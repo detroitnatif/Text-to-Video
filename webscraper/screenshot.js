a1058a0821b0dc7a44f8b0176bb3ecc67a8f9f2c
detroitnatif <tytydizzle124@gmail.com>
2024-03-17 15:47:59 +0100

    Web Scraper Puppeteer

--- webscraper/screenshot.js
+++ webscraper/screenshot.js
@@ -2,6 +2,22 @@ import puppeteer from 'puppeteer'
 
 (async () => {
 
-    const browser = puppeteer.browser
+    const browser = await puppeteer.launch({
+        headless: headless ? 'new' : false,
+    });
+
+    const page = await browser.newPage();
+
+    await page.setViewport(
+        {
+            width: 1200,
+            height: 1200,
+            deviceScaleFactor: 1,
+        }
+    );
+
+    page.goto('https://en.wikipedia.org/Sam_altman');
+
+    browser.close();
 }
 )();
