10a19d55b93018e3fd08e15f3100608c56bfe4d3
detroitnatif <tytydizzle124@gmail.com>
2024-03-28 09:34:57 +0100

    Automatic directory control

---
+++ videoCreation/images.py
@@ -0,0 +1,33 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+import narration
+import json
+import base64
+
+load_dotenv()
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+def create_from_data(data):
+    image_num = 0
+    for element in data:
+        if element['type'] != 'image':
+            continue
+        image_num += 1
+        image_name = f'image_{image_num}.webp'
+        generate(element['description'], os.path.join('images', image_name))
+
+def generate(prompt="POV like youve got a GoPro on looking down while cutting cutting an onion", output_file='creation.webp'):
+    response =  client.images.generate(
+        model='dall-e-3',
+        prompt=prompt,
+        size='1024x1024',
+        quality='standard',
+        n=1,
+        response_format='b64_json')
+
+    image_b64 = response.data[0].b64_json
+
+    with open(output_file, 'wb') as f:
+        f.write(base64.b64decode(image_b64))
+    return output_file
--- videoCreation/script.py
+++ videoCreation/script.py
@@ -55,3 +55,6 @@ with open('response.txt', 'w') as f:
 with open('data.json', 'w') as f:
     json.dump(data, f)
 
+if not os.path.exists('images'):
+    os.makedirs('images')
+
