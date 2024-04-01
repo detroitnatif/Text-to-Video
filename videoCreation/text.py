b7d810d7fe8caec2801a8d45bc4eb126250c2432
detroitnatif <tytydizzle124@gmail.com>
2024-04-01 09:40:27 +0200

    writing text onto videos, need to make it fit

--- videoCreation/text.py
+++ videoCreation/text.py
@@ -1,5 +1,6 @@
 import cv2
 
+
 def write_text(text, frame):
     font = cv2.FONT_HERSHEY_SIMPLEX
     white_color = (255, 255, 255)
@@ -17,25 +18,32 @@ def write_text(text, frame):
     frame = cv2.putText(frame, text, org, font, font_scale, white_color, thickness , cv2.LINE_AA)
 
 
+
+
+
+def caption(video, output_video, text, narration_time):
+    input_video = video
+    cap = cv2.VideoCapture(input_video)
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_video, fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
+
+    while cap.isOpened():
+    
+        ret, frame = cap.read()
+        if not ret:
+            break
+        write_text("this is a sample text", frame)
+        out.write(frame)
+
+
+    cap.release()
+    out.release()
+
+
 input_video = '/Users/tylerklimas/Desktop/openaisandbox/videoCreation/cooking_with_audio_plus.mp4'
 cap = cv2.VideoCapture(input_video)
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 output_video = 'withtranscript.avi'
 out = cv2.VideoWriter(output_video, fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
-
-
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
-    write_text("this is a sample text", frame)
-    out.write(frame)
-
-
-cap.release()
-out.release()
-
-
-while cap.isOpened():
-    ret, frame = cap.read()
